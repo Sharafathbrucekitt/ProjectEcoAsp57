ddf61829ceee6a60f9cbdf5a80279f61388dff88
Sharafathbrucekitt <120270427+Sharafathbrucekitt@users.noreply.github.com>
2024-12-06 08:11:34 +0000

    page created

---
+++ about.php
@@ -0,0 +1,112 @@
+<html >
+<head>
+    <title>About EFM</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 10px;
+             background-image: url('eco14.jpeg');
+              background-repeat: no-repeat;
+             background-position: center;
+             background-attachment: move;
+             background-size: cover; 
+            
+        }
+        header {
+
+            color: white;
+            padding: 30px;
+            text-align: center;
+        }
+          header h1 {
+            font-size: 36px;
+            letter-spacing: 1px;
+            }
+
+           nav a {
+            color: white;
+            margin:  15px;
+            text-decoration: none;
+            font-size: 17px;
+           
+           }
+
+          nav a:hover{
+            color:#FF7E5F;
+            }
+       
+        h2 {
+            font-size: 30px;
+            color:#0c4a04;
+            margin-bottom: 20px;
+            text-transform: uppercase;
+            border-bottom: 2px solid #0c4a04;
+            padding-bottom: 20px;
+           }
+
+        footer {
+            text-align: center;
+            padding: 10px;
+            margin-top: 20px;
+            background-color:#084524; 
+            color: white;
+        }
+
+    </style>
+</head>
+<body>
+<header>
+    
+    <h2>Eco-Friendly Marketplace</h2>
+    <h3><span style="color:#0c4a04;">About EFM</span></h3>
+    <h5><span style="color:#0c4a04;">Your one-stop online platform for sustainable and eco-friendly products!</span></h5>
+
+ <nav>
+        <a href="home.html" style="color:
+#702616 ; margin: 0 35px;">Home</a>
+        <a href="collections.php" style="color: 
+#702616; margin: 0 35px;">Collection</a>
+	<a href="login.html" style="color: 
+#702616; margin: 0 35px;">Login</a>	
+        <a href="contact.php" style="color:
+#702616; margin: 0 35px;">Contact</a>
+    </nav> 
+</header>
+
+<div class="container">
+    <br>
+    <br>
+    <h1><span style="color:Green;">What is Eco Friendly Market Place</span></h1>
+    <p>Welcome to our Eco-Friendly Product Marketplace! We are dedicated to promoting sustainable living by connecting buyers with eco-friendly products and sellers committed to environmental responsibility.<br>
+
+<br>The Eco-Friendly Marketplace project aims to establish an online platform dedicated to promoting and selling sustainable products. This initiative addresses the growing consumer demand for environmentally friendly options while supporting businesses committed to sustainability.<br><br>This marketplace specializes in zero-waste products, offering a variety of items designed to eliminate packaging waste. Eco Friendly Market Place not only sells eco-friendly goods but also educates consumers about sustainability practices.<br>
+
+<br>Explore a wide range of products, learn about their sustainability features, and contribute to a greener future. Join our community today!</p><br>
+
+<p><h1><span style="color:Green;">Vision</span></h1></p>
+    <p>To be the leading online marketplace that empowers consumers to make environmentally conscious choices, fostering a sustainable future for our planet</p>
+
+
+ <p><h1><span style="color:Green;">Mission</span></h1></p>
+    <p>Our mission is to provide a curated selection of sustainable products that meet the highest environmental standards, while educating and inspiring our community to adopt greener lifestyles. We strive to reduce waste, promote ethical sourcing, and support brands that prioritize the planet."</p>
+
+
+<p><h1><span style="color:Green;">Values of Eco Friendly market Place</span></h1></p>
+    <ul>
+         <li>Sustainability: Commitment to the Environment:</li>
+<br>
+     <li>Social Responsibility: Engage in initiatives that give back to the community.</li><br>
+        <li>High Standards for Products: Ensure that all items offered are of high quality</li><br>
+        <li>Open Communication: Maintain honesty about sourcing and pricing.</li><br>
+        <li>Continuous Improvement: Embrace creativity and innovation in product offerings.</li><br>
+       
+    </ul>
+</div>
+
+<footer>
+    <p>&copy; 2024 Zahh Fashion Store | All Rights Reserved</p>
+</footer>
+
+</body>
+</html>
